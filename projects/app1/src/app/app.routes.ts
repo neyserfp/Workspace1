afb49dce49e17648cd45f44d69007ad2ea86b6cb
neyserfp <neyser@digitechfp.com>
2024-02-28 12:41:49 +0100

    Creando pages (vistas)

--- projects/app1/src/app/app.routes.ts
+++ projects/app1/src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { Pagina1Component } from './pages/pagina1/pagina1.component';
+import { Pagina2Component } from './pages/pagina2/pagina2.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'home',
+        component: HomeComponent
+    },{
+        path: 'pagina1',
+        component: Pagina1Component
+    },{
+        path: 'pagina2',
+        component: Pagina2Component
+    }
+];
