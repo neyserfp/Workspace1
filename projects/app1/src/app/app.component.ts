8fce8abdcd391c68e95ad2a7173c5f2accc8bf24
neyserfp <neyser@digitechfp.com>
2024-02-28 13:22:45 +0100

    Cambios 2

--- projects/app1/src/app/app.component.ts
+++ projects/app1/src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
 import { InitialComponent } from './components/initial/initial.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, InitialComponent],
+  imports: [RouterOutlet, InitialComponent, RouterModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
